ab78b785dc983cc2515f22566f226bc1b9bf4f79
mehmetkocerr <46313560+mehmetkocerr@users.noreply.github.com>
2020-09-15 23:43:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
 });
 app.use(cors());
 
@@ -26,4 +27,4 @@ app.use(express.json());
 app.use('/api/auth/user', authRoutes);
 app.use('/api/', routes);
 
-app.listen(5000, () => console.log('server kalktı.'));
+app.listen(port, () => console.log('server kalktı.'));
