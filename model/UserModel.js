8db5a47fbe307abab4bf2af20cc709f1c639f677
mehmetkocerr <46313560+mehmetkocerr@users.noreply.github.com>
2020-09-15 23:53:14 +0300

    re-installing modules

--- model/UserModel.js
+++ model/UserModel.js
@@ -2,39 +2,39 @@ const sql = require('../config/Db.js');
 const user = {};
 
 user.Register = (UserData) => {
-  return new Promise((resolve, reject) => {
-    sql.query('INSERT INTO admin_user set ?', UserData, (err, res, fields) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(res);
-    });
-  });
+	return new Promise((resolve, reject) => {
+		sql.query('INSERT INTO admin_user set ?', UserData, (err, res, fields) => {
+			if (err) {
+				return reject(err);
+			}
+			resolve(res);
+		});
+	});
 };
 
 user.FindOne = (Email) => {
-  return new Promise((resolve, reject) => {
-    sql.query('SELECT * FROM admin_user WHERE email = ?', Email, (err, res) => {
-      // console.log(res[0]);
-      if (err) {
-        return reject({
-          status: false,
-          data: err,
-        });
-      } else {
-        if (res.length === 0) {
-          return resolve({
-            status: false,
-            data: res,
-          });
-        }
-        resolve({
-          status: true,
-          data: res,
-        });
-      }
-    });
-  });
+	return new Promise((resolve, reject) => {
+		sql.query('SELECT * FROM admin_user WHERE email = ?', Email, (err, res) => {
+			// console.log(res[0]);
+			if (err) {
+				return reject({
+					status: false,
+					data: err,
+				});
+			} else {
+				if (res.length === 0) {
+					return resolve({
+						status: false,
+						data: res,
+					});
+				}
+				resolve({
+					status: true,
+					data: res,
+				});
+			}
+		});
+	});
 };
 
 module.exports = user;
