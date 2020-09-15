1c0f85a4d96725187f4bce3accfb29c096f856a9
mehmetkocerr <46313560+mehmetkocerr@users.noreply.github.com>
2020-09-16 00:22:04 +0300

    Update CategoryModel.js

--- model/CategoryModel.js
+++ model/CategoryModel.js
@@ -1,16 +1,17 @@
-const sql = require('../config/db');
+const sql = require('../config/Db.js');
+
 exports.All = () => {
-  return new Promise((resolve, reject) => {
-    sql.query(
-      'SELECT * FROM category AS c INNER JOIN category_descrption AS ct ON c.categoryId= ct.categoryId',
-      (err, res) => {
-        // console.log(res[0]);
-        if (err) {
-          return reject(err);
-        } else {
-          resolve(res);
-        }
-      }
-    );
-  });
+	return new Promise((resolve, reject) => {
+		sql.query(
+			'SELECT * FROM category AS c INNER JOIN category_descrption AS ct ON c.categoryId= ct.categoryId',
+			(err, res) => {
+				// console.log(res[0]);
+				if (err) {
+					return reject(err);
+				} else {
+					resolve(res);
+				}
+			}
+		);
+	});
 };
