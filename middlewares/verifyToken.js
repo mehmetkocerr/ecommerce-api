3b4c720019edf0310872eec7c9513e2ea8ae46a9
mehmetkocerr <46313560+mehmetkocerr@users.noreply.github.com>
2020-09-16 00:37:52 +0300

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,13 +1,15 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = function auth(req, res, next) {
-  const token = req.header("Authorization");
-  if (!token) return res.status(401).send("Access Denied");
-  try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
-    next();
-  } catch (error) {
-    res.status(400).send("Invalid Token");
-  }
+	const token = req.header('Authorization');
+	if (!token) return res.status(401).send('Access Denied');
+	try {
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		req.user = verified;
+		next();
+	} catch (error) {
+		res.status(400).send('Invalid Token');
+	}
 };
